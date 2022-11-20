2e927bfae4dc6da4e6c1d134cc03ddc4b5277a56
Muhamet <muhametsadiku655@gmail.com>
2022-11-20 22:21:22 +0100

    Display all Orders

--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -7,24 +7,22 @@ $(document).ready(function () {
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": {
-            "url":"/Admin/Product/GetAll"
+            "url":"/Admin/Order/GetAll"
         },
         "columns": [
-            { "data": "title", "width": "15%" },
-            { "data": "isbn", "width": "15%" },
-            { "data": "price", "width": "15%" },
-            { "data": "author", "width": "15%" },
-            { "data": "category.name", "width": "15%" },
+            { "data": "id", "width": "15%" },
+            { "data": "name", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "applicationUser.email", "width": "15%" },
+            { "data": "orderStatus", "width": "15%" },
+            { "data": "orderTotal", "width": "15%" },
             {
                 "data": "id",
                 "render": function (data) {
                     return `
                         <div class="w-75 btn-group" role="group">
-                            <a href = "/Admin/Product/Upsert?id=${data}" class="btn btn-primary mx-2"> 
-                                <i class="bi bi-pencil-square"></i> Edit
-                            </a>
-                            <a onClick=Delete('/Admin/Product/Delete/${data}') class="btn btn-danger mx-2">
-                                <i class="bi bi-trash-fill"></i> Delete
+                            <a href = "/Admin/Order/Details?orderId=${data}" class="btn btn-primary mx-2"> 
+                                <i class="bi bi-pencil-square"></i> Details
                             </a>
                         </div>
                     `
@@ -33,32 +31,4 @@ function loadDataTable() {
             }
         ]
     });
-}
-
-function Delete(url) {
-    Swal.fire({
-        title: 'Are you sure',
-        text: "You won't be alble to revert this",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'yes, delete it!'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                url: url,
-                type: 'DELETE',
-                success: function (data) {
-                    if (data.success) {
-                        dataTable.ajax.reload();
-                        toastr.success(data.message);
-                    }
-                    else {
-                        toastr.error(data.message);
-                    }
-                }
-            })
-        }
-    })
 }
